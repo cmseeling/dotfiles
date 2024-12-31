59778c2eeaa22999b0cded68f5de35c9627d4730
cmseeling <cmseeling@gmail.com>
2024-12-31 17:42:11 -0600

    added lsd, nvim, yazi, updated fish and ghostty

--- Configs/fish/.config/fish/config.fish
+++ Configs/fish/.config/fish/config.fish
@@ -4,3 +4,19 @@ end
 
 source ~/.asdf/asdf.fish
 starship init fish | source
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+fzf --fish | source
+
+set -gx EDITOR nvim
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+abbr -a ls lsd -Al --tree --depth 1
+abbr -a ls-al lsd -Al --ignore-config
